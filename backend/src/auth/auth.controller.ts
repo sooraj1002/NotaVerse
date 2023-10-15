81140bc504bd080e775d5ef5f9b0f75c7a079646
soorajk1 <ksooraj162@gmail.com>
2023-10-16 02:21:19 +0530

    added dto to auth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login() {
+  login(@Body() dto: AuthDto) {
+    console.log(dto);
     return this.authService.login();
   }
 
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
